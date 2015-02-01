d0fe306d7c1a2380416bbd0a66311adc8df7cbb2
Sikae <sikabarca@gmail.com>
2015-02-01 17:24:52 -0500

    Add subclasses.

--- homework_0.py
+++ homework_0.py
@@ -8,10 +8,42 @@ class Multimedia:
     '''
     Main class for Movie, Song and VideoGame
     '''
-
-
     def __init__(self, params):
         '''
         Constructor
         '''
-        
+        pass
+
+    def getter_var1(self):
+        pass
+    
+    def setter_var1(self):
+        pass
+
+    def getter_var2(self):
+        pass
+    
+    def setter_var2(self):
+        pass
+
+class Video(Multimedia):
+    """
+    TODO: ...
+    """
+    def __init__(self):
+        pass
+    
+
+class Movie(Multimedia):
+    """
+    TODO: ...
+    """
+    def __init__(self):
+        pass
+    
+class Song(Multimedia):
+    """
+    TODO: ...
+    """
+    def __init__(self):
+        pass
